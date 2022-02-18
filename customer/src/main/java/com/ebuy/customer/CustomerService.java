cdcc2073085b87d9c787053248277289d25541bf
Rahul Gopu <rahulreddy.gopu96@gmail.com>
2022-02-18 09:26:07 -0600

    eureka-server & load-balanced rest template setup

--- customer/src/main/java/com/ebuy/customer/CustomerService.java
+++ customer/src/main/java/com/ebuy/customer/CustomerService.java
@@ -19,7 +19,7 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
--- fraud/src/main/java/com/ebuy/fraud/FraudCheckController.java
+++ fraud/src/main/java/com/ebuy/fraud/FraudCheckController.java
@@ -1,14 +1,17 @@
 package com.ebuy.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public record FraudCheckController(FraudCheckService fraudCheckService) {
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 
